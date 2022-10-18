f6a258a5f4b1f781407fd5adc252d8e7dd0246ab
CJMario89 <a7654321000@gmail.com>
2022-10-18 21:54:06 +0800

    BREAKING CHANG: move client folder into  server folder & npm run dev

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/public')));
+
+app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '/client/public', 'index.html'));
+    }
+)
+
+app.listen(PORT, () => console.log(`${ PORT } start to serve`))
